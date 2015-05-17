5db1578c8da0f012050d9122773d56e3bb4aaf99
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-16 22:27:09 -0700

    first commit

---
+++ LaserDot/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  LaserDot
+//
+//  Created by Zakk Hoyt on 5/16/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ LaserDot/MyScene.m
@@ -0,0 +1,13 @@
+//
+//  MyScene.m
+//  LaserDot
+//
+//  Created by Zakk Hoyt on 5/16/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import "MyScene.h"
+
+@implementation MyScene
+
+@end
