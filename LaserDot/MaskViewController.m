cd7d7c8dcf1a8739af1eb2a34e0e91a6ec569f3e
Zakk Hoyt <zacharyhoyt@Zacharys-iMac.local>
2015-05-30 18:09:27 -0700

    This app works as a video recorder with live GPUImage filtering. Saves to photos

--- LaserDot/MaskViewController.m
+++ LaserDot/MaskViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "MaskViewController.h"
+@import Photos;
+#import "PHAsset+Utility.h"
+
 
 
 @interface MaskViewController ()
@@ -16,16 +19,21 @@
     GPUImageMovieWriter *movieWriter;
 }
 @property (strong, nonatomic) GPUImageView *gpuImageView;
-
+@property (weak, nonatomic) IBOutlet UIButton *recordButton;
+@property (nonatomic, strong) PHPhotoLibrary *photos;
+@property (nonatomic, strong) NSURL *movieURL;
 @end
 
 @implementation MaskViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     
     self.gpuImageView = [[GPUImageView alloc]initWithFrame:self.view.bounds];
     [self.view addSubview:self.gpuImageView];
+    [self.gpuImageView addSubview:self.recordButton];
+    
     
     videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
     videoCamera.outputImageOrientation = UIInterfaceOrientationLandscapeRight;
@@ -36,57 +44,83 @@
     [videoCamera addTarget:filter];
     self.gpuImageView.fillMode = kGPUImageFillModePreserveAspectRatioAndFill;
     
-    // Record a movie for 10 s and store it in /Documents, visible via iTunes file sharing
-    
-//    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
-//    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
-//    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
-//    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0)];
-//    movieWriter.encodingLiveVideo = YES;
-//    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(640.0, 480.0)];
-//    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(720.0, 1280.0)];
-//    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(1080.0, 1920.0)];
-//    [filter addTarget:movieWriter];
     [filter addTarget:self.gpuImageView];
     
     [videoCamera startCameraCapture];
     
-//    double delayToStartRecording = 0.5;
-//    dispatch_time_t startTime = dispatch_time(DISPATCH_TIME_NOW, delayToStartRecording * NSEC_PER_SEC);
-//    dispatch_after(startTime, dispatch_get_main_queue(), ^(void){
-//        NSLog(@"Start recording");
-//        
-//        videoCamera.audioEncodingTarget = movieWriter;
-//        [movieWriter startRecording];
-//        
-//        //        NSError *error = nil;
-//        //        if (![videoCamera.inputCamera lockForConfiguration:&error])
-//        //        {
-//        //            NSLog(@"Error locking for configuration: %@", error);
-//        //        }
-//        //        [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOn];
-//        //        [videoCamera.inputCamera unlockForConfiguration];
-//        
-//        double delayInSeconds = 10.0;
-//        dispatch_time_t stopTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-//        dispatch_after(stopTime, dispatch_get_main_queue(), ^(void){
-//            
-//            [filter removeTarget:movieWriter];
-//            videoCamera.audioEncodingTarget = nil;
-//            [movieWriter finishRecording];
-//            NSLog(@"Movie completed");
-//            
-//            //            [videoCamera.inputCamera lockForConfiguration:nil];
-//            //            [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOff];
-//            //            [videoCamera.inputCamera unlockForConfiguration];
-//        });
-//    });
 
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)startRecording{
+    
+    NSString *pathToMovie = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/Movie.m4v"];
+    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+    self.movieURL = [NSURL fileURLWithPath:pathToMovie];
+    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:self.movieURL size:CGSizeMake(640.0, 480.0)];
+    movieWriter.encodingLiveVideo = YES;
+    [filter addTarget:movieWriter];
+
+    
+    
+    NSLog(@"Start recording");
+    
+    videoCamera.audioEncodingTarget = movieWriter;
+    [movieWriter startRecording];
+    
+    //        NSError *error = nil;
+    //        if (![videoCamera.inputCamera lockForConfiguration:&error])
+    //        {
+    //            NSLog(@"Error locking for configuration: %@", error);
+    //        }
+    //        [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOn];
+    //        [videoCamera.inputCamera unlockForConfiguration];
+    
+//    double delayInSeconds = 10.0;
+//    dispatch_time_t stopTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+//    dispatch_after(stopTime, dispatch_get_main_queue(), ^(void){
+//        
+//        
+//        //            [videoCamera.inputCamera lockForConfiguration:nil];
+//        //            [videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOff];
+//        //            [videoCamera.inputCamera unlockForConfiguration];
+//    });
+}
+
+-(void)stopRecording{
+    
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        [filter removeTarget:movieWriter];
+        videoCamera.audioEncodingTarget = nil;
+        [movieWriter finishRecording];
+        NSLog(@"Movie completed");
+
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [PHAsset saveVideoAtURL:self.movieURL location:nil completionBlock:^(PHAsset *asset, BOOL success) {
+                if(success){
+                    NSLog(@"Success");
+                } else {
+                    NSLog(@"Error");
+                }
+            }];
+        });
+    }];
+
+}
+
+- (IBAction)recordButtonTouchUpInside:(UIButton*)sender {
+    if([sender.titleLabel.text isEqualToString:@"Rec"]){
+        [sender setTitle:@"Stop" forState:UIControlStateNormal];
+        [self startRecording];
+    } else {
+        [sender setTitle:@"Rec" forState:UIControlStateNormal];
+        [self stopRecording];
+    }
 }
 
 /*
---
+++ LaserDot/PHAsset-Utility/README.md
@@ -0,0 +1,48 @@
+A category to simplify a few tasks for the PHAsset class. 
+
+Sample usage for saving an PHAsset to an album (will create the album if it doesn't exist):
+
+```
+PHAsset *asset = // however you are getting your PHAsset
+[asset saveToAlbum:@"My App Album" completionBlock:^(BOOL success) {
+    if(success){
+        // Your asset now resides in My App Album
+    } else {
+        // Something bad happened. Handle it here.
+    }
+}];
+```
+
+Sample usage for getting metadata from a PHAsset
+
+```
+PHAsset *asset = // however you are getting your PHAsset
+[asset requestMetadataWithCompletionBlock:^(NSDictionary *metadata) {
+    NSLog(@"Metadata: %@", metadata);
+}];
+```
+
+Sample: Save a UIImage to camera roll (returns PHAsset in completion block)
+
+```
+UIImage *image = [UIImage imageNamed:@"terribleImage"];
+[PHAsset saveImageToCameraRoll:image 
+                      location:nil 
+               completionBlock:^(PHAsset *asset, BOOL success) {
+                   NSLog(@"asset saved to camera roll");
+               }];
+```
+
+Save video (from NSURL) to camera roll (returns PHAsset in completion block)
+
+```
+NSURL *url = [NSURL urlWithString:@"terribleURL"];
+[PHAsset saveVideoAtURL:videoFileURL 
+               location:nil 
+        completionBlock:^(PHAsset *asset, BOOL success) {
+            NSLog(@"asset saved to camera roll");
+        }];
+```
+
+
+
